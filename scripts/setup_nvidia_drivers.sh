f2b3acdb7e3338fbe0dc7e604d1dd0421f990bd6
Aleksei <amenshih@lanta-net.ru>
2026-06-24 12:15:43 +0300

    fix: add -y option to the setup_nvidia_drivers.sh script

--- scripts/setup_nvidia_drivers.sh
+++ scripts/setup_nvidia_drivers.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-apt-get install linux-headers-$(uname -r)
+apt-get install -y linux-headers-$(uname -r)
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda-keyring_1.1-1_all.deb
